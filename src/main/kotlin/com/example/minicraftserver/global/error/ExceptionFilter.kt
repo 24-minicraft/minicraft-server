6dcae4c4d88e7537026dfc1cef63801ca02b236c
min56 <minchaeg85@gmail.com>
2024-04-12 13:22:49 +0900

    add:: ExceptionFilter

---
+++ src/main/kotlin/com/example/minicraftserver/global/error/ExceptionFilter.kt
@@ -0,0 +1,44 @@
+package com.example.minicraftserver.global.error
+
+import com.example.minicraftserver.global.error.exception.MinicraftException
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
+import kotlin.jvm.Throws
+
+@Component
+class ExceptionFilter(
+    private val objectMapper: ObjectMapper,
+) : OncePerRequestFilter() {
+
+    @Throws(IOException::class)
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain,
+    ) {
+        try {
+            filterChain.doFilter(request, response)
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+            when (exception) {
+                is MinicraftException -> writeErrorCode(exception, response)
+                else -> writeErrorCode(InternalServerError.EXCEPTION, response)
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun writeErrorCode(exception: MinicraftException, response: HttpServletResponse) {
+        val errorResponse = ErrorResponse.of(exception)
+
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.status = errorResponse.status
+        response.writer.write(objectMapper.writeValueAsString(errorResponse))
+    }
+}
