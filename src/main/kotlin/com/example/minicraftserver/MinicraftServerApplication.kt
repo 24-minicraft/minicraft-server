6ad9315c9c18b00f5d875980bd181d30c724f009
min56 <minchaeg85@gmail.com>
2024-04-16 09:20:47 +0900

    update:: @EnableConfigurationProperties 추가

--- src/main/kotlin/com/example/minicraftserver/MinicraftServerApplication.kt
+++ src/main/kotlin/com/example/minicraftserver/MinicraftServerApplication.kt
@@ -1,8 +1,11 @@
 package com.example.minicraftserver
 
+import com.example.minicraftserver.global.security.jwt.JwtProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
+@EnableConfigurationProperties(JwtProperties::class)
 @SpringBootApplication
 class MinicraftServerApplication
 
