93813ac495b6c53b276a6197e7c297c515b69faf
min56 <minchaeg85@gmail.com>
2024-05-06 22:08:25 +0900

    add:: TokenExpiredException

---
+++ src/main/kotlin/com/example/minicraftserver/global/exception/TokenExpiredException.kt
@@ -0,0 +1,8 @@
+package com.example.minicraftserver.global.exception
+
+import com.example.minicraftserver.global.error.exception.ErrorCode
+import com.example.minicraftserver.global.error.exception.MinicraftException
+
+object TokenExpiredException: MinicraftException(
+    ErrorCode.TOKEN_EXPIRED
+)
