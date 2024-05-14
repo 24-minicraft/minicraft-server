a49c66c988441396c9a5107f3f5f6e0df0bf3848
woonseok105 <yws3786@gmail.com>
2024-05-14 13:52:02 +0900

    add :: 자원 판매 리스트 조회 API

---
+++ src/main/kotlin/com/example/minicraftserver/domain/market/presentation/dto/response/QuerySellMaterialResponse.kt
@@ -0,0 +1,12 @@
+package com.example.minicraftserver.domain.market.presentation.dto.response
+
+import com.example.minicraftserver.global.enums.ItemType
+
+data class QuerySellMaterialsResponse(
+    val materials: List<Material>
+)
+
+data class Material (
+    val type: ItemType,
+    val price: Int
+)
