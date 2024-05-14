5f6a0c22fb9b4774747f38d369f1751796b8790c
alsco39 <minchaeg85@gmail.com>
2024-05-14 12:14:28 +0900

    update :: 연관관계 변경

--- src/main/kotlin/com/example/minicraftserver/domain/equipment/domain/Equipment.kt
+++ src/main/kotlin/com/example/minicraftserver/domain/equipment/domain/Equipment.kt
@@ -1,27 +1,36 @@
 package com.example.minicraftserver.domain.equipment.domain
 
+import com.example.minicraftserver.domain.character.domain.Character
+import com.example.minicraftserver.domain.user.domain.User
+import com.example.minicraftserver.global.entity.BaseIdEntity
 import com.example.minicraftserver.global.enums.ItemType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
-import jakarta.persistence.Id
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
+import jakarta.validation.constraints.NotNull
 
 @Table(name = "tbl_equipment")
 @Entity
 class Equipment(
-    @Id
+    id: Long,
+
     @Enumerated(EnumType.STRING)
     @Column(columnDefinition = "")
-    val itemType: ItemType,
+    val type: ItemType,
 
-    @Column(columnDefinition = "INT DEFAULT 0")
-    val health: Int,
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    val user: User,
 
-    @Column(columnDefinition = "INT DEFAULT 0")
-    val defense: Int,
-
-    @Column(columnDefinition = "INT DEFAULT 0")
-    val luck: Int,
-)
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "character_id")
+    val character: Character,
+): BaseIdEntity(id)
