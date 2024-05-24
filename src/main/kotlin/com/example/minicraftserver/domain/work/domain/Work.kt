8635085b81a08afd44eab5b2e0158778ff337802
min56 <minchaeg85@gmail.com>
2024-05-24 13:54:37 +0900

    update :: Enumrated 추가

--- src/main/kotlin/com/example/minicraftserver/domain/work/domain/Work.kt
+++ src/main/kotlin/com/example/minicraftserver/domain/work/domain/Work.kt
@@ -6,6 +6,8 @@ import com.example.minicraftserver.global.entity.BaseIdEntity
 import com.fasterxml.jackson.annotation.JsonFormat
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.Table
 import jakarta.validation.constraints.NotNull
 import java.time.LocalDateTime
@@ -13,13 +15,15 @@ import java.time.LocalDateTime
 @Table(name = "tbl_work")
 @Entity
 class Work (
-    id: Long,
+    id: Long = 0,
 
     @NotNull
-    @Column(columnDefinition = "VARCHAR(10) DEFAULT COLLECTION")
+    @Column(columnDefinition = "VARCHAR(10) DEFAULT 'COLLECTION'")
+    @Enumerated(EnumType.STRING)
     val workType: WorkType,
 
     @Column(columnDefinition = "VARCHAR(7)")
+    @Enumerated(EnumType.STRING)
     val regionType: RegionType,
 
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss", timezone="Asia/Seoul")
