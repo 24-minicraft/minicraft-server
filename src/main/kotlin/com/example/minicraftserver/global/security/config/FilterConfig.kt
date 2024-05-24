2f08928369fede1dd0dea4abdb77af9978725e25
min56 <minchaeg85@gmail.com>
2024-05-24 13:55:55 +0900

    update :: JwtFilter 추가

--- src/main/kotlin/com/example/minicraftserver/global/security/config/FilterConfig.kt
+++ src/main/kotlin/com/example/minicraftserver/global/security/config/FilterConfig.kt
@@ -1,16 +1,25 @@
 package com.example.minicraftserver.global.security.config
 
 import com.example.minicraftserver.global.error.ExceptionFilter
+import com.example.minicraftserver.global.security.jwt.JwtTokenFilter
+import com.example.minicraftserver.global.security.jwt.JwtTokenProvider
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.SecurityConfigurer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
-class FilterConfig (
-    val objectMapper: ObjectMapper,
-) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
+@Configuration
+class FilterConfig(
+    private val jwtTokeProvider: JwtTokenProvider,
+    private val objectMapper: ObjectMapper
+) : SecurityConfigurer<DefaultSecurityFilterChain, HttpSecurity> {
 
     override fun configure(builder: HttpSecurity) {
-        val exceptionFilter = ExceptionFilter(objectMapper)
+        builder.addFilterBefore(JwtTokenFilter(jwtTokeProvider), UsernamePasswordAuthenticationFilter::class.java)
+        builder.addFilterBefore(ExceptionFilter(objectMapper), JwtTokenFilter::class.java)
     }
+
+    override fun init(builder: HttpSecurity?) {}
 }
